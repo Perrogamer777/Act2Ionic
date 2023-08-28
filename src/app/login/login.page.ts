fcc251fcb93c93a9875cf9b23522a446d251d9a8
Perrogamer777 <lucho.man229@gmail.com>
2023-08-28 12:59:39 -0400

    no pudimos

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+
+
+  usuario={
+    usuario : ''
+    }
 
   constructor() {
     
